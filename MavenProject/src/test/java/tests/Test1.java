5895e5f19a5b9ee0eff0ebbc1f1c60345c57c02d
kodur <kodur@DESKTOP-PAJQIH8>
2019-10-25 10:54:43 -0400

    Test automation

---
+++ MavenProject/src/test/java/tests/Test1.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void printClass() {
+	  System.out.println("This is a print class");
+  }
+}
---
+++ MavenProject/src/test/java/tests/Test2.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test2 {
+  @Test(enabled=false)
+  public void printClass2() {
+	  System.out.println("This test should be ignored");
+  }
+}
